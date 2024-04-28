d10990ee8671e68b74d9fb073dcd2fd52bb22a3e
Matthew-Delurio <116933951+Matthew-Delurio@users.noreply.github.com>
2024-04-27 19:45:45 -0700

    Add files via upload

--- content.js
+++ content.js
@@ -1,28 +1,38 @@
-
-
-  
+toTranslate="";
   function wholePageTextReplacement(){
     temp = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,li,td,caption,span,a');
     for (let i = 0; i < temp.length; i++) {
-      temp[i].innerText="";
+      temp[i].innerText=""; //translate each one
     }
   }
+
+
+    const set = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, li, td, caption, span, a');
+    set.forEach(element => {
+        element.addEventListener("mouseup", function(){
+            toTranslate=window.getSelection().toString();
+            console.log(toTranslate);
+        });
+    });
+  
   
   function wholePageReversion(){
     location.reload();
   }
   
   function selectFunctionality(){
+    return toTranslate; // translate the text before returning
   }
 
+  
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log(request);
     if (request.message == "Whole Page Translation") {
         wholePageTextReplacement();
     }else if(request.message == "Selective Translation"){
         selectFunctionality();
+        sendResponse({ success: true, message: toTranslate });
     }else if(request.message == "Revert Page Translation"){
-        console.log("hi")
         wholePageReversion();
     }
   });
--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     var buttons = document.querySelectorAll('button');
     var selects = document.querySelector('select');
-  
+    var textarea = document.querySelector('textarea');
     // Add click event listener to each button
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener('click', function(){
@@ -18,12 +18,20 @@ document.addEventListener('DOMContentLoaded', function(){
           this.classList.add('opaque-button');
           buttons[i].disabled = true;
         }
+
         
         // Send message to the content script
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-          chrome.tabs.sendMessage(tabs[0].id,{message:buttons[i].innerHTML});
-          console.log("hello");
-        });
+          chrome.tabs.sendMessage(tabs[0].id, { message: buttons[i].innerHTML }, function (response) {
+              if (response) {
+                  console.log(response.message);
+                  textarea.innerText=response.message;
+              } else {
+                  console.log("No response received.");
+              }
+          });
+          console.log("Message sent.");
+      });
       });
     }
   });
